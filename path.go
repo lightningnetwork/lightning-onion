49867a538c5dcf3d612d37a733879e9d033c650b
Christian Decker <decker.christian@gmail.com>
2019-02-04 19:37:07 +0100

    sphinx: Introduce HopPayload as opaque, variable length, byte-slice
    We'll be encoding to and decoding from a variable length byte-slice
    once we complete the multi-hop proposal.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- path.go
+++ path.go
@@ -1,6 +1,12 @@
 package sphinx
 
-import "github.com/btcsuite/btcd/btcec"
+import (
+	"bytes"
+	"fmt"
+	"math"
+
+	"github.com/btcsuite/btcd/btcec"
+)
 
 // PaymentPath represents a series of hops within the Lightning Network
 // starting at a sender and terminating at a receiver. Each hop contains a set
@@ -11,6 +17,18 @@ import "github.com/btcsuite/btcd/btcec"
 // and unroll the extra data into the space used for route forwarding
 // information.
 type PaymentPath [NumMaxHops]OnionHop
+
+// HopPayload is a slice of bytes and associated payload-type that are
+// destined for a specific hop in the PaymentPath. The payload itself
+// is treated as an opaque datafield by the onion router, while the
+// Realm is modified to indicate how many hops are to be read by the
+// processing node. The 4 MSB in the realm indicate how many
+// additional hops are to be processed to collect the entire payload.
+type HopPayload struct {
+	Realm   [1]byte
+	Payload []byte
+	HMAC    [hmacSize]byte
+}
 
 // OnionHop represents an abstract hop (a link between two nodes) within the
 // Lightning Network. A hop is composed of the incoming node (able to decrypt
@@ -27,6 +45,29 @@ type OnionHop struct {
 	// HopData are the plaintext routing instructions that should be
 	// delivered to this hop.
 	HopData HopData
+
+	HopPayload HopPayload
+}
+
+// Helper function to transition from the two separate arrays to the
+// single struct that describes the entire path.
+func NewPaymentPath(nodeIds []*btcec.PublicKey, hopsData []HopData) (*PaymentPath, error) {
+	var paymentPath PaymentPath
+
+	if len(nodeIds) != len(hopsData) {
+		return nil, fmt.Errorf("node ID count does not match hop data count: %d != %d", len(nodeIds), len(hopsData))
+	}
+
+	for i := range nodeIds {
+		paymentPath[i].NodePub = *nodeIds[i]
+		paymentPath[i].HopData = hopsData[i]
+		// FIXME: use HopData.Encode to fill in the real payload
+		paymentPath[i].HopPayload = HopPayload{
+			Realm:   [1]byte{hopsData[i].Realm},
+			Payload: bytes.Repeat([]byte{0x00}, 32),
+		}
+	}
+	return &paymentPath, nil
 }
 
 // IsEmpty returns true if the hop isn't populated.
@@ -66,4 +107,35 @@ func (p *PaymentPath) TrueRouteLength() int {
 	}
 
 	return routeLength
+}
+
+func (p *PaymentPath) CountFrames() int {
+	frameCount := 0
+	for _, hop := range p {
+		if hop.IsEmpty() {
+			break
+		}
+		frameCount = frameCount + hop.HopPayload.CountFrames()
+	}
+	return frameCount
+}
+
+// CountFrames returns the number of frames required to encode a given payload
+func (hp *HopPayload) CountFrames() int {
+	// If it all fits in the legacy payload size, don't use any
+	// additional frames.
+	if len(hp.Payload) <= 32 {
+		return 1
+	}
+
+	// Otherwise we'll need at least one additional frame: subtract
+	// the 64 bytes we can stuff into payload and hmac of the
+	// first, and the 33 bytes we can pack into the payload of the
+	// second, then divide the remainder by 65.
+	remainder := len(hp.Payload) - 64 - 33
+	return 2 + int(math.Ceil(float64(remainder)/65))
+}
+
+func (hp *HopPayload) CalculateRealm() {
+	hp.Realm[0] = (hp.Realm[0] & 0x0F) | (byte(hp.CountFrames()-1) << 4)
 }
