56a62e1ff779259b8ff08733dfaffec14565921e
Olaoluwa Osuntokun <laolu32@gmail.com>
2022-02-10 17:55:15 -0800

    bench: use 20 hops for the benchmark
    Turns out in practice, the 27 hop limit is actually _too large_, so we
    fall back to the old 20 hop limit for now. We'll want to circle back and
    emperically derive the _true_ hop limit.

--- bench_test.go
+++ bench_test.go
@@ -21,7 +21,7 @@ func BenchmarkPathPacketConstruction(b *testing.B) {
 		route        PaymentPath
 	)
 
-	for i := 0; i < NumMaxHops; i++ {
+	for i := 0; i < 20; i++ {
 		privKey, err := btcec.NewPrivateKey()
 		if err != nil {
 			b.Fatalf("unable to generate key: %v", privKey)
